2004eda8726f27ed54e059a0939efe526a99fa76
Christian <christianpavilonis@outlook.com>
2022-01-19 08:57:35 -0600

    Initial commit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::{io, thread};
+use std::sync::mpsc;
+use std::thread::{current, sleep, sleep_ms};
+use std::time::Duration;
+
+use crossterm::{event, ExecutableCommand, terminal};
+use crossterm::cursor::{Hide, Show};
+use crossterm::event::{Event, KeyCode};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use invaders::frame;
+use invaders::frame::new_frame;
+use invaders::render::render;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a thread
+    let  (render_transceiver, render_receiver) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let current_frame = match render_receiver.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+
+            render(&mut stdout, &last_frame, &current_frame, false);
+            last_frame = current_frame;
+        }
+    });
+
+    'gameloop: loop {
+        // let current_frame = new_frame();
+
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        // let _ = render_transceiver.send(current_frame);
+        // thread::sleep(Duration::from_millis(1))
+    }
+
+    //Cleanup
+    // render_handle.join().unwrap();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
